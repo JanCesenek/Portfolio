da6f77e683fca135b666db20e55f84c8f94ea177
JanCesenek <jan.cesenekCZ@gmail.com>
2023-01-30 15:30:12 +0100

    optimized for all screen sizes

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
       <div className="Align">
         <Routes>
           <Route
+            exact
             path="/"
             element={
               <Page
--- src/components/Bubble/Bubble.js
+++ src/components/Bubble/Bubble.js
@@ -4,9 +4,13 @@ import Stargate from "../../imgs/Stargate.png";
 
 const Bubble = (props) => {
   const [hovered, setHovered] = useState(false);
+  const [clicked, setClicked] = useState(false);
 
   const frontPartRule = hovered ? classes.BackPart : classes.FrontPart;
   const backPartRule = hovered ? classes.FrontPart : classes.BackPart;
+
+  const frontPartClicked = clicked ? classes.BackPart : classes.FrontPart;
+  const backPartClicked = clicked ? classes.FrontPart : classes.BackPart;
 
   const containerRule =
     !hovered && props.visibilityStatus
@@ -24,26 +28,49 @@ const Bubble = (props) => {
   };
 
   return (
-    <div className={containerRule} onMouseEnter={enterAnimation} onMouseLeave={exitAnimation}>
-      <h1 className={frontPartRule}>{props.title}</h1>
-      <p className={backPartRule}>
-        {props?.content}
-        <br />
-        {props.contact && (
-          <span>
-            <i className="fas fa-envelope"></i>
-            jan.cesenekCZ@gmail.com
-            <br />
-            <i className="fas fa-phone"></i>
-            +420 773 140 855
-          </span>
-        )}
-        {props.link && (
-          <a href={process.env.PUBLIC_URL + props.link} target="_blank" rel="noreferrer">
-            <img src={Stargate} alt="link" className={classes.Link} />
-          </a>
-        )}
-      </p>
+    <div>
+      <div className={containerRule} onMouseEnter={enterAnimation} onMouseLeave={exitAnimation}>
+        <h1 className={frontPartRule}>{props.title}</h1>
+        <p className={backPartRule}>
+          {props?.content}
+          <br />
+          {props.contact && (
+            <span>
+              <i className="fas fa-envelope"></i>
+              jan.cesenekCZ@gmail.com
+              <br />
+              <i className="fas fa-phone"></i>
+              +420 773 140 855
+            </span>
+          )}
+          {props.link && (
+            <a href={process.env.PUBLIC_URL + props.link} target="_blank" rel="noreferrer">
+              <img src={Stargate} alt="link" className={classes.Link} />
+            </a>
+          )}
+        </p>
+      </div>
+      <div className={classes.ContainerCopy} onClick={() => setClicked(!clicked)}>
+        <h1 className={frontPartClicked}>{props.title}</h1>
+        <p className={backPartClicked}>
+          {props?.content}
+          <br />
+          {props.contact && (
+            <span>
+              <i className="fas fa-envelope"></i>
+              jan.cesenekCZ@gmail.com
+              <br />
+              <i className="fas fa-phone"></i>
+              +420 773 140 855
+            </span>
+          )}
+          {props.link && (
+            <a href={process.env.PUBLIC_URL + props.link} target="_blank" rel="noreferrer">
+              <img src={Stargate} alt="link" className={classes.Link} />
+            </a>
+          )}
+        </p>
+      </div>
     </div>
   );
 };
